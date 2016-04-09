5a990e075ae6fbff820ecc53a230f194936c632a
Giancarlo Franchini <gfranchini88@gmail.com>
2016-04-09 00:05:15 -0400

    fixed script

--- build.sh
+++ build.sh
@@ -4,18 +4,16 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 echo "Installing Ruby..."
-sudo apt-get update
 sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 cd
 wget http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz
-#tar -xzvf ruby-2.2.3.tar.gz (this contains verbose mode)
-tar -xzf ruby-2.2.3.tar.gz
+tar -xzvf ruby-2.2.3.tar.gz (this contains verbose mode)
 cd ruby-2.2.3/
 ./configure
 make
 sudo make install
 echo "Installing Bundler..."
-gem install bundler
+sudo gem install bundler
 echo "Installing Git..."
 sudo apt-get -y install git
 git config --global color.ui true
@@ -25,7 +23,7 @@ echo "Installing NodeJS..."
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 echo "Installing Rails..."
-gem install rails -v 4.2.4 --no-rdoc
+sudo  gem install rails -v 4.2.4
 echo "Installing MySQL..."
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
