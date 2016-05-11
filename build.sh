556ab2f33d59a48469bf0a0469926344bcc6fc83
Giancarlo  Franchini <gfranchini88@gmail.com>
2016-05-11 11:55:14 -0400

    removed stupid comment

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Installing Ruby..."
 sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 cd
 wget http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz
-tar -xzvf ruby-2.2.3.tar.gz (this contains verbose mode)
+tar -xzvf ruby-2.2.3.tar.gz
 cd ruby-2.2.3/
 ./configure
 make
